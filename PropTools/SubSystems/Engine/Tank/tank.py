f0585f3a68c8307ccb2b20a79ed5be3d5aff0a42
Tom Aveyard <thomasaveyard@gmail.com>
2021-09-01 17:07:10 +0100

    Added tank object

--- PropTools/SubSystems/Engine/Tank/tank.py
+++ PropTools/SubSystems/Engine/Tank/tank.py
@@ -3,10 +3,6 @@ from PropTools.SubSystems.Engine.Propellant.propellant import Propellant
 # Defines state of propellant in the tank from the stated temperature and pressure of the tank
 class Tank(Propellant):
 
-    def __init__(self, propellant, temp, pressure):
-        
-        # Initialises super class
-        super().__init__(propellant)
-        self.defineState("T", temp, "P", pressure)
+    def __init__(self, outletState:Propellant):
 
-        self.totalTankVolume = None
+        self.outletState = outletState
